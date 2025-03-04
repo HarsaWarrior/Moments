803816a83ca1db370683ac14b9eb4ad1ccbc247f
Harsh Patil <harsh3152002@gmail.com>
2025-03-04 11:53:10 +0530

    Started developing Frontend for the Application. Created React App using vite and installed necessary libraries

---
+++ Frontend/Moments/src/Pages/Authentication/Authentication.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Authentication extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <div>Authentication</div>
+        );
+    }
+}
+ 
+export default Authentication;
