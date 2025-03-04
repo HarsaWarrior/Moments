a0bcbe92b45eaf54ba003d369dc9349aeef5e8e0
Harsh Patil <harsh3152002@gmail.com>
2025-03-05 01:28:22 +0530

    Created basic authentication page layout and design

--- Frontend/Moments/src/Pages/Authentication/Authentication.jsx
+++ Frontend/Moments/src/Pages/Authentication/Authentication.jsx
@@ -1,10 +1,19 @@
+import { Card } from '@mui/material';
 import React, { Component } from 'react';
+
 
 class Authentication extends Component {
     state = {  } 
     render() { 
         return (
-            <div>Authentication</div>
+            <div className="w-screen h-screen bg-[#1A1A1D] bg-[url('/Hero.jpg')] bg-cover bg-center flex justify-center items-center">
+                <div className='h-[600px] w-[400px] md:h-[900px] md:w-[800px] bg-gradient-to-r from-amber-50/50 to-cyan-50/50 rounded-3xl backdrop-blur-sm'>
+                    <div>
+                        <h1 className='font-serif md:text-4xl antialiased italic tracking-wide text-[#1A1A1D] text-center uppercase mt-20'>Moments</h1>
+                        <h3 className='md:text-xl tracking-wide text-[#1A1A1D] text-center uppercase m-6 animate-pulse'>Capture life’s moments, one story at a time.</h3>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
